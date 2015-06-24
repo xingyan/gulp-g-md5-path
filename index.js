9f30e0e8b4eb00e30dcfd11865c2145ff2e5ed83
zzming <zzming98@qq.com>
2015-06-24 21:21:18 +0800

    fix this.push

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ module.exports = function() {
         files: file.path
       });
 
+    file.oldPath = file.path;
     file.path = (output && output[0] && output[0].md5) ? path.join(dirName, output[0].md5 + extName) : file.path;
 
-    this.push(file);
-
     cb(null, file);
+    
   });
 }
